1fdcb56a56b9228ab255eb2359a469937dc42723
frank <franciskipchumba5@gmail.com>
2017-09-17 21:24:05 +0300

    crete user models and sign up method

---
+++ shoppin_list_app/shopping_app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import render_template, redirect
+from models import User
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello world"
+
+@app.route('/sign-up', method=['POST','GET'])
+def create_account():
+    user_name = None
+    email = None
+    user_password = None
+    error = None
+
+    """
+    GET request displays sign-up form. 
+    POST request registers the current user
+    """
+    if request.method == "POST":
+        user_name = request.form['user_name']
+        email = request.form['email']
+        user_password = request.form['user_password']
+
+        if email in User.email_index:
+            error = "A user with that email already exist"
+            return render_template('sign-up.html', error=error)
+        else:
+            user = User(user_name, email, user_password)
+            return redirect(url_for('login'))
+        else:
+            return render_template('sign-up.html', error=error)
