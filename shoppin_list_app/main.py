c3e8ff02cd264c9886849e0cd873e864aedf12a7
frank <franciskipchumba5@gmail.com>
2017-09-19 14:24:25 +0300

    [Feature #151119486] user can create account

--- shoppin_list_app/main.py
+++ shoppin_list_app/main.py
@@ -2,15 +2,32 @@ from flask import Flask
 from flask import (
     render_template,
     redirect,
+    session,
     request,
-    url_for)
+    url_for,
+    flash)
+
 from models import User
 
 app = Flask(__name__)
+app.secret_key = 'asdfzxcvqwer'
 
-# @app.route("/")
-# def hello_world():
-#     return "hello world"
+@app.route("/")
+def hello_world():
+    user = session.get('email')
+    print(user)
+    return render_template('home.html')
+
+# @app.route('/register' , methods=['GET','POST'])
+# def register():
+#     if request.method == 'GET':
+#         return render_template('register.html')
+#     user = User(request.form['username'] , request.form['password'],request.form['email'])
+#     print(request.form['username'])
+#     # session[username] = request.form['username']
+#     # session.commit()
+#     flash('User successfully registered')
+#     return redirect(url_for('hello_world'))
 
 @app.route('/sign-up', methods=['POST','GET'])
 def create_account():
@@ -23,16 +40,36 @@ def create_account():
     GET request displays sign-up form. 
     POST request registers the current user
     """
-    if request.method == "POST":
-        user_name = request.form['user_name']
-        email = request.form['email']
-        user_password = request.form['user_password']
 
-        if email in User.email_index:
-            error = "A user with that email already exist"
-            return render_template('sign-up.html', error=error)
-        else:
-            user = User(user_name, email, user_password)
-            return redirect(url_for('login'))
-    else:
-        return render_template('sign-up.html', error=error)
+    if request.method == 'GET':
+        return render_template('sign-up.html')
+    user = User(request.form['username'] , request.form['password'],request.form['email'])
+    # session[username] = request.form['username']
+    # session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('hello_world'))
+
+
+    # if request.method == "POST":
+    #     user_name = request.form['user_name']
+    #     email = request.form['email']
+    #     user_password = request.form['user_password']
+
+    #     if email in User.email_index:
+    #         error = "A user with that email already exist"
+    #         return render_template('sign-up.html', error=error)
+    #     else:
+
+    #         user = User(user_name, email, user_password)
+    #         print(user)
+    #         return "done"
+    #         # return redirect(url_for('hello_world'))
+    # else:
+    #     return render_template('sign-up.html', error=error)
+
+# @app.route('/login', methods=['POST', 'GET'])
+# def login():
+#     pass
+
+if __name__ =='__main__':
+    app.run( debug=True)
--- shoppin_list_app/test_app.py
+++ shoppin_list_app/test_app.py
@@ -1,16 +1,21 @@
 import os
-import main
+import flask
 import unittest
-import tempfile
+from main import create_account
+
+app = flask.Flask(__name__)
 
 class ShoppingAppTestCase(unittest.TestCase):
     def set_up(self):
-        main.testing = True
-        self.app = main.test_client()
+        self.create_account = create_account
 
-    def test_sign_up(self):     
-        rv = self.app.get('/sign-up')
-        assert b'test case' in rv.data
+
+    def test_sign_up(self, username, email, password):
+    	return self.create_account.post('/sign-up', data=dict(
+    		username=username,
+    		email=email,
+    		password=password), follow_redirect=True)
+
 
 if __name__ == '__main__':
     unittest.main()
