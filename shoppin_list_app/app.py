ea372e01745550c2b09fca051aca4cb21d462002
frank <franciskipchumba5@gmail.com>
2017-09-26 01:26:11 +0300

    [chore]Codeclean up and comments

--- shoppin_list_app/app.py
+++ shoppin_list_app/app.py
@@ -39,58 +39,81 @@ def product_page(slist_id):
     if request.method == 'GET':
 
         slist = current_app.store.get_slist(slist_id)
+
         return render_template('home.html', slist=slist)
 
     else:
 		slist_ids = request.form.getlist('slist_ids')
+
 		for slist_id in slist_ids:
+
 			current_app.store.delete_slist(int(slist_id))
+
 		flash('%d movies deleted.' % len(slist_ids))
+
     return redirect(url_for('site.product_page'))
 
 @site.route('/shop/add', methods=['GET', 'POST'])
 def slist_add_page():
+    '''
+    The get method will create a new form
+
+    '''
     if request.method == 'GET':
         form = {'title': ''}
 
     else:
+        # validat the data in the forms
         valid = validate_data(request.form)
+
         if valid:
             title = request.form.data['title']
+            # add to class
             slist = ShoppingList(title)
+
             current_app.store.add_slist(slist)
+
             return redirect(url_for('site.home_page'))
+
         form = request.form
     return render_template('add_shopping_list.html', form=form)
 
 @site.route('/shop/<int:slist_id>/edit', methods=['GET', 'POST'])
 # @login_required
 def slist_edit_page(slist_id):
+
     slist = current_app.store.get_slist(slist_id)
+
     if request.method == 'GET':
-        form = {'title': slist.title,
-                'year': slist.year if slist.year else ''}
-        return render_template('add_shopping_list.html.html', form=form,
-                               min_year=1887, max_year=datetime.now().year)
+
+        form = {'title': slist.title}
+
+        return render_template('add_shopping_list.html.html', form=form )
+
     else:
+
         slist.title = request.form['title']
-        slist.year = int(request.form['year']) if request.form['year'] else None
+
         current_app.store.update_movie(slist)
-        flash('Movie data updated.')
-        return redirect(url_for('site.product_page', slist_id=movie._id))
+        flash('list  data updated.')
+        return redirect(url_for('site.product_page', slist_id=slist._id))
 
 
 @site.route('/login', methods=['GET', 'POST'])
 def login_page():
+
     form = LoginForm()
+
     if form.validate_on_submit():
 
         username = form.data['username']
+
         user = get_user(username)
 
         if user is not None:
 
             password = form.data['password']
+
             if pwd_context.verify(password, user.password):
 
                 login_user(user)
@@ -100,34 +123,29 @@ def login_page():
                 next_page = request.args.get('next', url_for('site.home_page'))
 
                 return redirect(next_page)
+
         flash('Invalid credentials.')
+
     return render_template('sign-in.html', form=form)
 
 @site.route('/logout')
 def logout_page():
+
     logout_user()
+
     flash('You have logged out.')
+
     return redirect(url_for('site.home_page'))
 
 
 def validate_data(form):
     form.data = {}
+
     form.errors = {}
 
     if len(form['title'].strip()) == 0:
         form.errors['title'] = 'Title can not be blank.'
     else:
         form.data['title'] = form['title']
-
-    # if not form['year']:
-    #     form.data['year'] = None
-    # elif not form['year'].isdigit():
-    #     form.errors['year'] = 'Year must consist of digits only.'
-    # else:
-    #     year = int(form['year'])
-    #     if (year < 1887) or (year > datetime.now().year):
-    #         form.errors['year'] = 'Year not in valid range.'
-    #     else:
-    #         form.data['year'] = year
 
     return len(form.errors) == 0
